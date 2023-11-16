45a955758b9db26f1732be61aca02b380f0c4b2b
Daniel Imfeld <daniel@imfeld.dev>
2023-11-15 22:08:14 -1000

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Should've Been a Blog Post
+
+So many YouTube videos take forever to watch and you feel like you could have just read the transcript instead. This
+tool helps with that, using Whisper to obtain a high quality transcript and placing images alongside the text so you
+don't miss out on the visuals too.
+
+This is a work in progress. The core data pipeline works but the presentation side needs to be written.
---
+++ src/sbbp/__init__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/eny python3
+from pprint import pp
+import json
+import sys
+import torch
+from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor, pipeline
+
+
+def run():
+    device = (
+        "cuda:0"
+        if torch.cuda.is_available()
+        else "mps"
+        if torch.backends.mps.is_available()
+        else "cpu"
+    )
+    torch_dtype = torch.float32 if device == "cpu" else torch.float16
+
+    model_id = "distil-whisper/distil-large-v2"
+
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+    )
+    model.to(device)
+
+    processor = AutoProcessor.from_pretrained(model_id)
+
+    pipe = pipeline(
+        "automatic-speech-recognition",
+        model=model,
+        tokenizer=processor.tokenizer,
+        feature_extractor=processor.feature_extractor,
+        max_new_tokens=128,
+        chunk_length_s=15,
+        batch_size=16,
+        torch_dtype=torch_dtype,
+        device=device,
+        return_timestamps=True,
+    )
+
+    result = pipe(sys.argv[1])
+
+    pp(json.dumps(result))
