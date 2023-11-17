8450d2501a834a6c65388b70c697f95f6dd254ae
Daniel Imfeld <daniel@imfeld.dev>
2023-11-16 21:13:52 -1000

    simple theming

--- viewer/src/routes/+page.svelte
+++ viewer/src/routes/+page.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
   import { enhance } from '$app/forms';
+  import { Button } from '$lib/components/ui/button';
+  import { Input } from '$lib/components/ui/input';
+  import { Label } from '$lib/components/ui/label';
 
   export let data;
 </script>
 
-<form method="POST" use:enhance class="flex gap-2">
-  <label class="flex gap-2 flex-1 max-w-[100ch]">
-    <p>Add new path</p>
-    <input type="text" name="path" class="flex-1" />
-  </label>
-  <button type="submit">Add</button>
-</form>
+<main class="p-4">
+  <form method="POST" use:enhance class="flex flex-col gap-2 rounded-lg border border-border p-4">
+    <Label class="flex gap-2 flex-1 max-w-[100ch] text-base" for="path">Add new path</Label>
+    <div class="flex gap-2">
+      <Input type="text" name="path" class="flex-1" />
+      <Button type="submit">Add</Button>
+    </div>
+  </form>
 
-<ul>
-  {#each data.items as item (item.id)}
-    <li><a href="/docs/{item.id}">{item.title}</a> - {item.duration}</li>
-  {/each}
-</ul>
+  <ul class="flex flex-col gap-4">
+    {#each data.items as item (item.id)}
+      <li><a href="/docs/{item.id}" class="underline">{item.title}</a> - {item.duration}</li>
+    {/each}
+  </ul>
+</main>
